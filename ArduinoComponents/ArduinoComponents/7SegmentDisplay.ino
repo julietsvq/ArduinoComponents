1bf932c8f0fda7c59f408bd7cc04186429143dd1
julietsvq <julietmoreiro@hotmail.com>
2016-03-29 21:02:45 +0200

    7SegmentDisplay initial commit

---
+++ ArduinoComponents/ArduinoComponents/7SegmentDisplay.ino
@@ -0,0 +1,31 @@
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+int p = 8;
+int d4 = 9;
+int d3 = 10;
+int d2 = 11;
+int d1 = 12;
+
+void setup() {
+	pinMode(d1, OUTPUT);    
+	pinMode(d2, OUTPUT);    
+	pinMode(d3, OUTPUT);    
+	pinMode(d4, OUTPUT);    
+	pinMode(a, OUTPUT);    
+	pinMode(b, OUTPUT);    
+	pinMode(c, OUTPUT);    
+	pinMode(d, OUTPUT);    
+	pinMode(e, OUTPUT);    
+	pinMode(f, OUTPUT);    
+	pinMode(g, OUTPUT);    
+	pinMode(p, OUTPUT);
+}
+
+void loop() {
+
+}
